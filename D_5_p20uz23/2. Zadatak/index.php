fcd759cb651440d1e066e7d74e9c7cdfb4838c06
adamdervisevic <adamdervisevic@yahoo.com>
2023-04-19 08:34:48 +0200

    drugi zadatak

---
+++ D_5_p20uz23/2. Zadatak/index.php
@@ -0,0 +1,10 @@
+<?php
+    $n = 10;
+    $a = 2;
+    $drugiDan = $a + 2;
+    $preostalaPoglavlja = $n - $a - ($a + 2);
+    
+    echo "<hr>";
+    echo "Citaocu su preostala " . $preostalaPoglavlja . " poglavlja da procita.";
+    echo "<hr>"
+?>
