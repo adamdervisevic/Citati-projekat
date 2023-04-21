30ae9c9b34b87ea2ad9bffd344bb3a90869935d5
adamdervisevic <adamdervisevic@yahoo.com>
2023-04-21 08:56:38 +0200

    ispravak prvog zadatka

--- D_5_p20uz23/1. Zadatak/index.php
+++ D_5_p20uz23/1. Zadatak/index.php
@@ -1,13 +1,17 @@
 <?php
     $farenhajt = 100;
-    $celzijus = floor(($farenhajt - 32) * 5 / 9);
+    $celzijus = ($farenhajt - 32) * 5 / 9;
     $kelvin = $celzijus + 273.15;
     
     echo "<hr>";
-    echo "Temperatura u Celzijusima je: " . $celzijus;
+    echo "Temperatura u Kelvinima je: " . $kelvin;
     echo "<hr>"; 
 
+    $kelvin = 100;
+    $farenhajt = ($kelvin - 273.15) * 9/5 + 32;
+    
     echo "<hr>";
-    echo "Temperatura u Kelvinima je: " . $kelvin;
-    echo "<hr>";   
+    echo "Temperatura u Fatanhajtima je: " . $farenhajt;
+    echo "<hr>";
+
 ?>
