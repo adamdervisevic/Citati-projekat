e6321a0ab911781f3145b656aafbfba728036365
adamdervisevic <adamdervisevic@yahoo.com>
2023-04-21 09:04:09 +0200

    ispravak drugog zadatka

--- D_5_p20uz23/3. Zadatak/idex.php
+++ D_5_p20uz23/3. Zadatak/idex.php
@@ -1,17 +1,17 @@
 <?php
 
-    $pera = 1000;
-    $mika = 500;
-    $kusur = 200;
-    $dzemper = ($pera + $mika + $kusur) / 2;
-    $kusurPera = $pera - $dzemper;
-    $kusurMika = $mika - $dzemper;
+    $p = 2500;
+    $m = 2500;
+    $k = 500;
+    $dz = ($p + $m - $k) / 2;
+    $k_p = $p - $dz;
+    $k_m = $m- $dz;
 
     echo "<hr>";
-    echo "Kusur koji Pera dobije je " . $kusurPera;
+    echo "Kusur koji Pera dobije je " . $k_p;
     echo "<hr>";
 
     echo "<hr>";
-    echo "Kusur koji Mika dobije je " . $kusurMika;
+    echo "Kusur koji Mika dobije je " . $k_m;
     echo "<hr>"
 ?>
